b2b1458322ed89c1ac8c87aa3d2e85dc962238fa
Alex <xroker@gmail.com>
2021-02-07 17:01:49 -0800

    v1.3.2

--- app/src/main/java/by/aleks/ghcwidget/LoginActivity.java
+++ app/src/main/java/by/aleks/ghcwidget/LoginActivity.java
@@ -2,7 +2,7 @@ package by.aleks.ghcwidget;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.webkit.CookieManager;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
